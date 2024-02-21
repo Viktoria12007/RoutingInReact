ce15b678f54cfcfa1341198dd70573f6de74544d
Виктория Голубова <v.golubova@denexy.com>
2024-02-21 19:05:08 +0400

    Added exports for PlayListsPage and PlayListInfoPage.

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,5 @@
 export { MainPage } from "./MainPage";
 export { UsersPage } from "./UsersPage";
 export { UserInfoPage } from "./UserInfoPage";
+export { PlayListsPage } from "./PlayListsPage";
+export { PlayListInfoPage } from "./PlayListInfoPage";
