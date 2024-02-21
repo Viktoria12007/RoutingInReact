99449b9beeb804398c8c67b3d427ccbf4c5c6e97
Виктория Голубова <v.golubova@denexy.com>
2024-02-21 18:45:41 +0400

    The contents of the data directory files have been changed.

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,1 +1,2 @@
 export { USERS } from "./users";
+export { PLAYLISTS } from "./playlists";
--- src/data/interfaces.ts
+++ src/data/interfaces.ts
@@ -5,4 +5,21 @@ export interface IUser {
 	jobTitle: string;
 	avatar: string;
 	bio: string;
+	playlist?: TPlaylist;
 }
+
+interface IPlaylistFull {
+	id: number;
+	genre: string;
+	name: string;
+	songs: string[];
+}
+
+interface IPlaylistEmty {
+	id: number;
+	genre: "Non Music";
+	name: "";
+	songs: [];
+}
+
+export type TPlaylist = IPlaylistFull | IPlaylistEmty;
